a8091832c825f95a79d8d3061f3c77a850dfbfc3
Robert Atkey <bob.atkey@gmail.com>
2017-09-26 13:19:33 +0100

    Delete commented out code

--- editor/editor_main.ml
+++ editor/editor_main.ml
@@ -2,13 +2,3 @@ let _ =
   Ulmus.Component.attach
     ~parent_id:"editor"
     (module Editor)
-
-(*    (module VCR.Of
-        (Editor)
-        (struct
-          type t = Editor.action
-          let relevant = function
-            | Editor.Movement _ -> false
-            | _                 -> true
-         end))
-*)
