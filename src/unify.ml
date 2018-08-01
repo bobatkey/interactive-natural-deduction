6e0467d443a4e796028a1a80472b33ab3d531f9a
Robert Atkey <bob.atkey@gmail.com>
2018-08-01 16:11:26 +0100

    Improve unification algorithm

--- src/unify.ml
+++ src/unify.ml
@@ -1,24 +1,24 @@
-type mvar  = string
+type mvar = string
+
 type const = string
 
-type head =
-  | Var   of int
-  | Const of const
-
-(* FIXME: should we allow variables to be applied to anything? Why not
-   just plain variables on their own? Then defining substitution would
-   be easier. *)
 type tm =
-  | Bind of tm
-  | Appl of head * tm list
-  | Meta of mvar * int list
+  | Var  of int
+  | Con of const * binding_tm list
+  | Unk of mvar * int list
 
+and binding_tm =
+  { binders : int
+  ; term    : tm
+  }
+
+let nobind term = { binders = 0; term }
 
 (**********************************************************************)
-let lam m   = Appl (Const "lam", [Bind m])
-let var i   = Appl (Var i, [])
-let app m n = Appl (Const "app", [m; n])
-let const c = Appl (Const c, [])
+let lam m   = Con ("lam", [{binders = 1; term = m}])
+let var i   = Var i
+let app m n = Con ("app", [nobind m; nobind n])
+let const c = Con (c, [])
 
 (* The ground term '(\x. f x) (x y)' *)
 let redex =
@@ -26,7 +26,13 @@ let redex =
 
 (* The pattern *)
 let redex_pat =
-  app (lam (Meta ("X", []))) (Meta ("Y", []))
+  app (lam (Unk ("X", [0]))) (Unk ("Y", []))
+
+let pat =
+  app (Unk ("X", [])) (Unk ("X", []))
+
+let tm =
+  app (lam (var 0)) (lam (var 0))
 
 (**********************************************************************)
 (* Preparation of a term to be a definition *)
@@ -36,28 +42,26 @@ let prepare_var vars offset i =
     let i = i - offset in
     let rec find idx = function
       | [] ->
-         failwith "preparation failed: variables don't match up"
+         failwith "preparation failed: inaccessible variable"
       | j :: js ->
          if i = j then idx else find (idx+1) js
     in
     find 0 vars + offset
 
-let prepare_head vars offset = function
-  | Var i   -> Var (prepare_var vars offset i)
-  | Const c -> Const c
-
 let rec prepare vars offset = function
-  | Bind m ->
-     Bind (prepare vars (offset+1) m)
-  | Appl (h, ms) ->
-     Appl (prepare_head vars offset h,
-           List.map (prepare vars offset) ms)
-  | Meta (mv, vars) ->
+  | Var i ->
+     Var (prepare_var vars offset i)
+  | Con (c, ms) ->
+     Con (c, List.map (prepare_binding_tm vars offset) ms)
+  | Unk (mv, vars) ->
      (* FIXME: do an occurs check here *)
      (* FIXME: could do pruning here: if any of the variables required
         are not accessble, then create new metavariables to replace
         the old ones. *)
-     Meta (mv, List.map (prepare_var vars offset) vars)
+     Unk (mv, List.map (prepare_var vars offset) vars)
+
+and prepare_binding_tm vars offset { binders; term } =
+  { binders; term = prepare vars (offset+binders) term }
 
 (**********************************************************************)
 (* Instantiation of metavariable definitions *)
@@ -65,28 +69,29 @@ let instantiate_var vars offset i =
   if i < offset then i
   else List.nth vars (i - offset) + offset
 
-let instantiate_head vars offset = function
-  | Var i   -> Var (instantiate_var vars offset i)
-  | Const c -> Const c
-
 let rec instantiate vars offset = function
-  | Bind m ->
-     Bind (instantiate vars (offset+1) m)
-  | Appl (h, ms) ->
-     Appl (instantiate_head vars offset h,
-           List.map (instantiate vars offset) ms)
-  | Meta (mv, vars) ->
-     Meta (mv, List.map (instantiate_var vars offset) vars)
+  | Var i ->
+     Var (instantiate_var vars offset i)
+  | Con (c, ms) ->
+     Con (c, List.map (instantiate_binding_tm vars offset) ms)
+  | Unk (mv, vars) ->
+     Unk (mv, List.map (instantiate_var vars offset) vars)
+
+and instantiate_binding_tm vars offset { binders; term } =
+  { binders; term = instantiate vars (offset+binders) term }
 
 (**********************************************************************)
 (* Substituting for a metavariable *)
-let rec subst_mv (mv, defn as binding) = function
-  | Bind m ->
-     Bind (subst_mv binding m)
-  | Appl (h, ms) ->
-     Appl (h, List.map (subst_mv binding) ms)
-  | Meta (mv', vs) ->
-     if mv = mv' then instantiate vs 0 defn else Meta (mv', vs)
+let rec subst_mv (mv, m as defn) = function
+  | Var i ->
+     Var i
+  | Con (c, ms) ->
+     Con (c, List.map (subst_mv_binding defn) ms)
+  | Unk (mv', vs) ->
+     if mv = mv' then instantiate vs 0 m else Unk (mv', vs)
+
+and subst_mv_binding defn { binders; term } =
+  { binders; term = subst_mv defn term }
 
 (* ?X[x,y,z] := c(x,y)
 
@@ -107,19 +112,15 @@ let rec subst_mv (mv, defn as binding) = function
    _         =?= _               ==>   _|_
 *)
 
-let head_equal h1 h2 =
-  match h1, h2 with
-    | Var i,   Var j    -> i = j
-    | Const c, Const c' -> String.equal c c'
-    | _,       _        -> false
-
 let rec zip_problems ms ms' problems =
   match ms, ms' with
     | [],    []      -> Ok problems
-    | m::ms, m'::ms' ->
-       (match zip_problems ms ms' problems with
-         | Ok problems -> Ok ((m,m')::problems)
-         | Error err   -> Error err)
+    | {binders = b1; term = m1}::ms, {binders = b2; term = m2}::ms' ->
+       if b1 <> b2 then Error "binder mismatch"
+       else
+         (match zip_problems ms ms' problems with
+           | Ok problems -> Ok ((m1,m2)::problems)
+           | Error err   -> Error err)
     | _,     _       ->
        Error "length mismatch"
 
@@ -132,19 +133,19 @@ let update_problems binding =
 let rec unify subst = function
   | [] ->
      Ok subst
-  | (Meta (mv, mvvs), m) :: problems
-  | (m, Meta (mv, mvvs)) :: problems ->
+  | (Unk (mv, mvvs), m) :: problems
+  | (m, Unk (mv, mvvs)) :: problems ->
      (* FIXME: occurs check *)
      let m        = prepare mvvs 0 m in
-     let binding  = (mv, m) in
-     let subst    = binding :: update_subst binding subst in
-     let problems = update_problems binding problems in
+     let defn     = (mv, m) in
+     let subst    = defn :: update_subst defn subst in
+     let problems = update_problems defn problems in
      unify subst problems
-  | (Bind m, Bind m') :: problems ->
-     unify subst ((m,m') :: problems)
-  | (Appl (h, ms), Appl (h', ms')) :: problems when head_equal h h' ->
+  | (Var i, Var j) :: problems when i = j ->
+     unify subst problems
+  | (Con (c, ms), Con (c', ms')) :: problems when String.equal c c' ->
      (match zip_problems ms ms' problems with
        | Ok problems -> unify subst problems
        | Error err   -> Error err)
   | _ ->
-     Error "unification failed"
+     Error "unification failed: term mismatch"
